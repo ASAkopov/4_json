a4665d5a3798e760b3774832f76eba72e6bd03ec
ASAkopov <asakopov@gmail.com>
2018-03-24 17:14:49 +0300

    24.03.2018

--- pprint_json.py
+++ pprint_json.py
@@ -2,12 +2,15 @@ import json
 
 
 def load_data(filepath):
-    pass
-
+    with open(filepath, 'r') as fid:
+        jsondata = json.load(fid)
+    return jsondata
 
 def pretty_print_json(data):
-    pass
+    print(json.dumps(data, ensure_ascii = False, indent = 4, sort_keys = True))
 
 
 if __name__ == '__main__':
-    pass
+    filepath = "alco.json"
+    data = load_data(filepath)
+    pretty_print_json(data)
