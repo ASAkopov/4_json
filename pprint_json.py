b83558eb26846a4c76721175cc4fec2ccdade685
ASAkopov <asakopov@gmail.com>
2018-03-26 15:16:34 +0300

    "26.03.2018 pep check"

--- pprint_json.py
+++ pprint_json.py
@@ -4,12 +4,14 @@ import json
 
 def load_data(filepath):
     with open(filepath, 'r') as fid:
-        json_data = json.load(fid)        
+        json_data = json.load(fid)
     return json_data
 
 
 def pretty_print_json(json_data):
-    print(json.dumps(json_data, ensure_ascii=False, indent=4, sort_keys=True))
+    print(json.dumps(
+                    json_data, ensure_ascii=False,
+                    indent=4, sort_keys=True))
 
 
 if __name__ == '__main__':
