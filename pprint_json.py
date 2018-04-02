2cd429964871800dea4e94ad608c5f9cabff1de3
ASAk <asakopov@gmail.com>
2018-04-02 18:42:21 +0300

    02.04.18 minimize if statement with sys.exit()

--- pprint_json.py
+++ pprint_json.py
@@ -1,9 +1,11 @@
+#!/usr/bin/env python3
+
 import sys
 import json
 
 
 def load_data(filepath):
-    with open(filepath, 'r') as file_object:
+    with open(filepath, "r") as file_object:
         json_object = json.load(file_object)
     return json_object
 
@@ -17,19 +19,19 @@ def pretty_print_json(object_to_print):
     ))
 
 
-if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        file_path = sys.argv[1]
-        try:
-            jsonobject = load_data(file_path)
-        except IOError:
-            print("Can not open file.")
-        except ValueError:
-            print(
-                "Impossiple to deserialize JSON. "
-                "Possible inconsistency with format."
-            )
-        else:
-            pretty_print_json(jsonobject)
-    else:
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
         print("Filepath was not specified.")
+        sys.exit(0)
+    file_path = sys.argv[1]
+    try:
+        jsonobject = load_data(file_path)
+    except IOError:
+        print("Can not open file.")
+    except ValueError:
+        print(
+            "Impossiple to deserialize JSON. "
+            "Possible inconsistency with format."
+            )
+    else:
+        pretty_print_json(jsonobject)
